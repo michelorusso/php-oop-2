93aad54cad673f7fc53896ddf3f3061cb23e11d7
Michele Lorusso <kirito102095@hotmail.it>
2021-06-04 17:44:06 +0200

    Complete

--- index.php
+++ index.php
@@ -13,30 +13,31 @@ require_once __DIR__ . '/Electronic.php';
 require_once __DIR__ . '/User.php';
 
 // Book
-$harry_potter = new Book('Harry Potter', 19.5);
+$harry_potter = new Book('Harry Potter and the Sorcerer\'s Stone', 18.5);
 
 // Music/Film/Tv
 $queen = new MusicFilmTv('Bohemian Rhapsody', 20);
 $queen->category = 'Movie';
 
 // Videogames/Console
-$godOfWar = new VideogamesConsole('Good Of War' , 70);
+$godOfWar = new VideogamesConsole('God of War III Remastered Hits - PlayStation 4' , 79);
 $godOfWar->category = 'Videogames';
 
 // Electronic
-$tv = new Electronic('Samsung', 350);
+$tv = new Electronic('Samsung TV AU7190 Smart TV 43”, Crystal UHD, Wi-Fi, Titan Gray, 2021', 1399);
 
 // User
-$users = new User('Gino', 'Bianchi', 'ginobianchio@live.com');
-$users->addCart($godOfWar);
-
+$user = new User('Gino', 'Bianchi', 'ginobianchio@live.com');
+$user->addCart($godOfWar);
+$user->addCart($harry_potter);
+$user->addCart($tv);
 
 
 // var_dump($harry_potter->getInfoData());
 // var_dump($queen->getInfoData());
 // var_dump($godOfWar->getInfoData());
 // var_dump($tv->getInfoData());
-// var_dump($users);
+// var_dump($user);
 
 
 
@@ -58,7 +59,7 @@ $users->addCart($godOfWar);
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?ts=<?=time()?>&quot">
 
 </head>
 <body>
@@ -86,6 +87,10 @@ $users->addCart($godOfWar);
 
             <select name="category" id="category">
                 <option value="AllCategory">Tutte le Categorie</option>
+                <option value="Book">Book</option>
+                <option value="Music">Music/Film/Tv</option>
+                <option value="VideogamesConsole">Videogames and Console</option>
+                <option value="Electronic">Electronic</option>
             </select>
 
             <input id="search" type="text">
@@ -96,12 +101,59 @@ $users->addCart($godOfWar);
         <!-- List -->
         <div class="list">
             <span>Ciao Gino</span>
-            <div>Account & Liste</div>
+            <div class="country">Account e liste <i class="fas fa-sort-down"></i></div>
         </div>
         <!-- End List -->
 
+        <!-- Order -->
+        <div class="order">
+            <span>Resi</span>
+            <div class="country">e Ordini</div>
+        </div>
+        <!-- End Order -->
+
+        <!-- Cart -->
+        <div class="cart">
+            <i class="fas fa-shopping-cart"></i>
+            <span>Carrello</span>
+        </div>
+        <!-- End Cart -->
     </header>
     <!-- Header -->
+
+    <!-- MAIN -->
+    <main>
+
+        <h2>Riepilogo Ordine</h2>
+
+        <ul>
+        
+            <?php foreach($user->getCart() as $item) { ?>
+            <li>
+                
+                <b><?php echo $item->typology ?><br></b>
+                <?php echo $item->category ?><br>
+                <span>Nome Prodotto :</span>  <?php echo $item->name?>
+                <div class="price">
+                   <span>Prezzo :</span>  <?php echo $item->price ?> €
+                </div>
+                
+
+            </li>
+            <?php } ?>
+
+        </ul>
+
+        <div class="total">
+            <span>Totale: </span>
+            <?php 
+            echo $harry_potter->price +
+            $godOfWar->price +
+            $tv->price ?> €
+        </div>
+
+    </main>
+    <!-- MAIN -->
     
 </body>
 </html>
